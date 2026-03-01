b087907f3ec0c3a4e77cdaf6c9923718c0226f8f
sahriar nur <shariarnurnahin99@gmail.com>
2026-03-01 22:26:58 +0600

    frontend and project setup

---
+++ frontend/vite.config.ts
@@ -0,0 +1,17 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+// https://vite.dev/config/
+export default defineConfig({
+  server: {
+    port: 8080,
+    host: true,
+  },
+  plugins: [
+    react({
+      babel: {
+        plugins: [["babel-plugin-react-compiler"]],
+      },
+    }),
+  ],
+});
